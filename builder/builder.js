21a3832df93188b360070e99a2feb171bd0998a9
Noel Schmidt <noel_schmidt@outlook.de>
2023-03-17 19:26:06 +0100

    initial commit

---
+++ builder/builder.js
@@ -0,0 +1,25 @@
+import esbuild from "esbuild";
+import copyStaticFiles from "esbuild-copy-static-files"
+
+esbuild.build({
+    bundle: true,
+    target: "esnext",
+    format: "esm",
+    logLevel: "info",
+    minify: true,
+    entryPoints: ["src/main.ts"],
+    outfile: "dist/main.js",
+    external: [
+        /**
+         * Add external folders
+         * EXAMPLE:
+         *
+         * copyStaticFiles({
+         *       src: "src/views",
+         *       dest: "dist/views"
+         * }),
+         *
+         */
+    ],
+    plugins: []
+})
